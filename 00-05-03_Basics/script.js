585a6f5b4a15e7f568002c042654267971962cd6
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-06 12:25:56 +0600

    warming up practicing and learning the pre-requisits before starting the concept of OOPs

---
+++ 00-05-03_Basics/script.js
@@ -0,0 +1,24 @@
+let body = document.querySelector("body");
+let style = body.style.backgroundColor = "black"
+body.style.color = "#eee"
+
+
+// Learnin the prerequisits
+
+// Learning the basic concept before jumping into OOPs
+
+// start practicing the concept 
+
+let date = new Date()
+console.log(date);
+
+let obj = {
+    name: "MKL",
+    age: 21,
+    passion: "bkaiti",
+    introduceMySelf: function(){
+        console.log("hello");
+    }
+}
+
+console.log(obj)  
