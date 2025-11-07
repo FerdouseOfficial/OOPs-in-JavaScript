36dfbee57c607ba49a0ef8d10d5695a159a2f5fc
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-07 18:23:05 +0600

    The problem here is the introduceMyself is taking different different space to work or store data. To leaarn about more check out the commet okay

--- 00-05-03_Basics/Prototype.js
+++ 00-05-03_Basics/Prototype.js
@@ -1,7 +1,20 @@
 function Student(name, age) {
-    this.name = name,
-    this.age = age
+  (this.name = name), (this.age = age);
+  this.introduceMyself = function () {
+    console.log(`Hello, My name is ${this.name}. My age is ${this.age}`);
+  };
 }
 
 let s1 = new Student("Arafat", "11.3");
-console.log(s1)
+console.log(s1);
+s1.introduceMyself();
+
+let s2 = new Student("Sumaiya", "20");
+console.log(s2);
+s2.introduceMyself();
+
+// Its working fine but the introduceMyself Function is taking different different space to store. 
+
+// that's happening just because of We don't use ProtoType here.
+
+// here is the solution 
