4deb29619b5697eb0b98d1520643265d0efcf746
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-07 20:56:37 +0600

    start writing imporatnt notes in commets make sure to check out

--- OOPS/4PillarsPractice.js
+++ OOPS/4PillarsPractice.js
@@ -7,22 +7,26 @@
 // JavaScript, while not a purely class-based language like Java or C++, is heavily object-oriented and supports OOP principles through its prototype-based model and class syntax.
 
 //                      Class:
-                // A blueprint or template, encapsulating data (properties) and functions (methods)
+// A blueprint or template, encapsulating data (properties) and functions (methods)
 
-                //      OBJECT:
-                    // Instance of a class; each object has its own unique set of values for its properties
-
-
+//      OBJECT:
+// Instance of a class; each object has its own unique set of values for its properties
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                        // Class & Object
+// Class & Object
 
-                    // 4 Pillars of OOPs (Object Oriented Programming)
-                        // 1. Abstraction
-                        // 2. Encapsulation
-                        // 3. Inheritance
-                        // 4. Polymorphism 
-
+// 4 Pillars of OOPs (Object Oriented Programming)
+// 1. Abstraction
+// 2. Encapsulation
+// 3. Inheritance
+// 4. Polymorphism
 
 // ======================================================================================================================================================================================
 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// 1. Abstraction == Hiding complexity and showing only th essential features.
+// 2. Encapsulation == hiding data inside objects and provide security.
+// 3. Inheritance == using properties and methods from anotehr object/class. :- Its comes from DRY- (Don't Repeat Your Code ) concepts so that we don't need to write same thing so many times okay
+// 4. Polymorphism == same method behaving differently based on the object.
+
+//  If you ever have any problem to understand go for this 2 hours 4 minutes Times Stamps okay ======================================================================================================================================================================================
