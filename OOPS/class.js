f53992a1c1b057b9df763aeb44141cd4ae741be1
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-07 20:36:31 +0600

    I have created index.html to practice the concept in browswer console okay

--- OOPS/class.js
+++ OOPS/class.js
@@ -7,7 +7,27 @@
 // JavaScript, while not a purely class-based language like Java or C++, is heavily object-oriented and supports OOP principles through its prototype-based model and class syntax.
 
 //                      Class:
-                // A blueprint or template, encapsulating data (properties) and functions (methods)
+// A blueprint or template, encapsulating data (properties) and functions (methods)
 
-                //      OBJECT:
-                    // Instance of a class; each object has its own unique set of values for its properties
+//      OBJECT:
+// Instance of a class; each object has its own unique set of values for its properties
+
+class Car {
+  constructor(name, color, mileage) {
+    this.name = name;
+    this.color = color;
+    this.mileage = mileage;
+  }
+
+  start() {
+    console.log(`${this.name} car is starting....`);
+  }
+
+  stop() {
+    console.log(`${this.name} car is stopping.....`);
+  }
+}
+
+let Bmw = new Car("BMW", "Dark Blue", 45);
+let Buggati = new Car("Buggati", "Brown", 5);
+let Toyota = new Car("Toyota", "Silver", 28);
