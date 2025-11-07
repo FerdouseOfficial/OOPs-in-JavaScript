9df05f2bc6fc8333d69d57e70a57b41fbaf46cc7
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-07 18:33:24 +0600

    practicing the crucial concepts okay

--- 00-05-03_Basics/Prototype.js
+++ 00-05-03_Basics/Prototype.js
@@ -1,20 +1,48 @@
-function Student(name, age) {
-  (this.name = name), (this.age = age);
-  this.introduceMyself = function () {
-    console.log(`Hello, My name is ${this.name}. My age is ${this.age}`);
-  };
-}
+// function Student(name, age) {
+//   (this.name = name), (this.age = age);
+//   this.introduceMyself = function () {
+//     console.log(`Hello, My name is ${this.name}. My age is ${this.age}`);
+//   };
+// }
 
-let s1 = new Student("Arafat", "11.3");
-console.log(s1);
-s1.introduceMyself();
+// let s1 = new Student("Arafat", "11.3");
+// console.log(s1);
+// s1.introduceMyself();
 
-let s2 = new Student("Sumaiya", "20");
-console.log(s2);
-s2.introduceMyself();
+// let s2 = new Student("Sumaiya", "20");
+// console.log(s2);
+// s2.introduceMyself();
 
 // Its working fine but the introduceMyself Function is taking different different space to store. 
 
 // that's happening just because of We don't use ProtoType here.
 
 // here is the solution 
+
+// function Student(name, age) {
+//   (this.name = name), (this.age = age);
+
+//   // here you can apply the prototype
+//   Student.prototype.introduceMyself = function () {
+//     console.log(`Hello, My name is ${this.name}. My age is ${this.age}`);
+//   };
+// }
+
+// // by this way
+
+// // Student.prototype.introduceMyself
+
+// let s1 = new Student("Arafat", "11.3");
+// console.log(s1);
+// s1.introduceMyself();
+
+// let s2 = new Student("Sumaiya", "20");
+// console.log(s2);
+// s2.introduceMyself();
+
+
+// Lets practice more okay. 
+
+// By creating a back system okay:---------
+
+function BankAccount()
