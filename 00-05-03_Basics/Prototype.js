093f8a051a9627fa9de5af01a000b95ae0a31c67
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-07 18:38:54 +0600

    Try to convert every code you write in TypeScript okay

--- 00-05-03_Basics/Prototype.js
+++ 00-05-03_Basics/Prototype.js
@@ -13,11 +13,11 @@
 // console.log(s2);
 // s2.introduceMyself();
 
-// Its working fine but the introduceMyself Function is taking different different space to store. 
+// Its working fine but the introduceMyself Function is taking different different space to store.
 
 // that's happening just because of We don't use ProtoType here.
 
-// here is the solution 
+// here is the solution
 
 // function Student(name, age) {
 //   (this.name = name), (this.age = age);
@@ -40,9 +40,31 @@
 // console.log(s2);
 // s2.introduceMyself();
 
-
-// Lets practice more okay. 
+// Lets practice more okay.
 
 // By creating a back system okay:---------
 
-function BankAccount()
+function BankAccount(holdersName, balance = 0) {
+  (this.holdersName = holdersName),
+    (this.balance = balance),
+    (BankAccount.prototype.Deposit = function (balance) {
+      this.balance += balance;
+    }),
+    BankAccount.prototype,
+    (Withdraw = function (balance) {
+      this.balance -= balance;
+    });
+}
+
+let Arafat = new BankAccount("Arafat Ali", 150);
+console.log(Arafat)
+Arafat.Deposit(400);
+console.log(Arafat)
+
+let Kuhili = new BankAccount("Kuhili Khatun", 1000);
+console.log(Kuhili);
+Kuhili.Withdraw(200);
+console.log(Kuhili);
+
+
+// Try to convert every code you write in TypeScript okay
