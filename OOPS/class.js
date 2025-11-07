e53bef6b9f9b04c0208fbdb5023145579494c962
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-07 20:28:10 +0600

    adding imp comments make sure to check it out

---
+++ OOPS/class.js
@@ -0,0 +1,13 @@
+// Programming Paradigm:- means a way of programming
+
+// Object-Oriented Programming (OOPs) in JavaScript is a programming paradigm based on the concept of objects.
+
+// These objects encapsulate both data (attributes) and the functions that operate on the data (methods).
+
+// JavaScript, while not a purely class-based language like Java or C++, is heavily object-oriented and supports OOP principles through its prototype-based model and class syntax.
+
+//                      Class:
+                // A blueprint or template, encapsulating data (properties) and functions (methods)
+
+                //      OBJECT:
+                    // Instance of a class; each object has its own unique set of values for its properties
