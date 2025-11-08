e95754cc6d98a560f1d3614cb95a822cf83a5a3b
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-08 18:40:59 +0600

    I have write the comments Imporatnat notes about Inheritance inside it okay

---
+++ OOPS/Inheritance&Polymorphism.js
@@ -0,0 +1,10 @@
+// Inheritance and Polymorphism :------------
+
+// Inheritance is an  concept where one class (child) can acquire properties and methods of another class (parent).
+
+// 1. Reuse exiting code 
+// 2. create logical relationships (is-a)
+// 3. Reduce duplication 
+// 4. Easier maintenace and scalability 
+
+// Actually that's how it works okay. Okay 
