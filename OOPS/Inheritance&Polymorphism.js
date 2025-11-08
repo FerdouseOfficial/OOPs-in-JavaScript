e745e1ebb1a94d2134fc562edecf4e273198f8c3
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-08 18:58:38 +0600

    linking he js file to practice in cosole

--- OOPS/Inheritance&Polymorphism.js
+++ OOPS/Inheritance&Polymorphism.js
@@ -5,6 +5,45 @@
 // 1. Reuse exiting code 
 // 2. create logical relationships (is-a)
 // 3. Reduce duplication 
-// 4. Easier maintenace and scalability 
+// 4. Easier maintenace and scalability \
+
+// 5. Most useful for DRY 
 
 // Actually that's how it works okay. Okay 
+
+
+// start practicing okay // 
+
+class Car{
+    constructor(brand){
+        this.brand = brand;
+    }
+
+    start() {
+        console.log("Car is starting.......... ");
+    }
+    stop() {
+        console.log("Car is stopping..........")
+    }
+    drive() {
+        console.log(`${this.brand} is driving.....`)
+    }
+}
+
+class ElectricCar extends Car {
+    constructor(brand, battery) {
+        this.brand = brand;
+        this.battery = battery;
+    }
+    drive() {
+        console.log(`${this.brand} is driving silently with ${this.battery}% battery`);
+    }
+    charge() {
+        console.log(`${this.brand} is charging......`);
+    }
+
+}
+
+const Tesla = new ElectricCar("Tesla", 80);
+
+console.log(Tesla)
