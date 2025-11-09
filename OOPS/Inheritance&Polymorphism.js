88fb7ecb4dbe6cf51a86ec085aa3a447f8099de6
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-09 08:31:46 +0600

    addding very important comments about inheritance and I have clearly learned what is derived class and what is the super class do

--- OOPS/Inheritance&Polymorphism.js
+++ OOPS/Inheritance&Polymorphism.js
@@ -19,42 +19,44 @@
 
 // we can call it parent class
 // Or we can call it supe class 
-class Car{
-    constructor(brand){
-        this.brand = brand;
-    }
+// class Car{
+//     constructor(brand){
+//         this.brand = brand;
+//     }
 
 
-    start() {
-        console.log("Car is starting.......... ");
-    }
-    stop() {
-        console.log("Car is stopping..........")
-    }
-    drive() {
-        console.log(`${this.brand} is driving.....`)
-    }
-}
+//     start() {
+//         console.log("Car is starting.......... ");
+//     }
+//     stop() {
+//         console.log("Car is stopping..........")
+//     }
+//     drive() {
+//         console.log(`${this.brand} is driving.....`)
+//     }
+// }
 
 // the ElectricCar is a derived class which is build from the CAR okay
 // we can call it child class or derived class okay
-class ElectricCar extends Car {
-    constructor(brand, color,battery) {
-        super(brand, color) // call parent using super keyword
-        // super keyword is used to call the parent class and send them value.
-        this.battery = battery;
-    }
-    drive() {
-        console.log(`${this.brand} is driving silently with ${this.battery}% battery`);
-    }
-    charge() {
-        console.log(`${this.brand} is charging......`);
-    }
+// class ElectricCar extends Car {
+//     constructor(brand, color,battery) {
+//         super(brand, color) // call parent using super keyword
+//         // super keyword is used to call the parent class and send them value.
+//         this.battery = battery;
+//     }
+//     drive() {
+//         console.log(`${this.brand} is driving silently with ${this.battery}% battery`);
+//     }
+//     charge() {
+//         console.log(`${this.brand} is charging......`);
+//     }
 
-}
+// }
  
-const Tesla = new ElectricCar("Tesla", 80);
-console.log(Tesla)
+// const Tesla = new ElectricCar("Tesla", 80);
+// console.log(Tesla)
 // Tesla.drive()
 // Tesla.start()
 // Tesla.charge()
+
+
