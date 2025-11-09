a38db6bb5a8fa6e146d9b1050d7808e4a6cbdc19
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-09 08:36:23 +0600

    write very important notes about polymorphism in comments make sure to read it okay

--- OOPS/Inheritance&Polymorphism.js
+++ OOPS/Inheritance&Polymorphism.js
@@ -2,28 +2,26 @@
 
 // Inheritance is an  concept where one class (child) can acquire properties and methods of another class (parent).
 
-// 1. Reuse exiting code 
+// 1. Reuse exiting code
 // 2. create logical relationships (is-a)
-// 3. Reduce duplication 
+// 3. Reduce duplication
 // 4. Easier maintenace and scalability \
 
-// 5. Most useful for DRY 
+// 5. Most useful for DRY
 
-// Actually that's how it works okay. Okay 
+// Actually that's how it works okay. Okay
 
-
-// start practicing okay // 
+// start practicing okay //
 
 // Please try to add extra feature here okay
-// Also convert them into typescript code 
+// Also convert them into typescript code
 
 // we can call it parent class
-// Or we can call it supe class 
+// Or we can call it supe class
 // class Car{
 //     constructor(brand){
 //         this.brand = brand;
 //     }
-
 
 //     start() {
 //         console.log("Car is starting.......... ");
@@ -52,13 +50,23 @@
 //     }
 
 // }
- 
+
 // const Tesla = new ElectricCar("Tesla", 80);
 // console.log(Tesla)
 // Tesla.drive()
 // Tesla.start()
 // Tesla.charge()
 
+// PolyMorPhism
 
-// PolyMorPhism 
+// Poly = many ; morph = forms, Polymorphism - many forms
 
+// its allows different classes to define methods with the same name but differnt behavior. (or we can say tha has more than one form)
+
+// Imagine a play() button
+
+// On a video, It plays the video
+// On a music player, it plays the music
+// On a game, it starts the game okay
+
+    
